83c99ed7494f579711975afd346951df5c22d94d
MichelineWL <michelinewijaya@gmail.com>
2024-08-28 15:45:07 +0700

    feat: set up url deployment

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vec#zh4cysnh9#luth2endxw+b#@m-c4%po!2_asv$0*-nd54v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "micheline-wijaya-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
